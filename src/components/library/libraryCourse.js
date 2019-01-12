1bb04d09f59e6518565a58ffb04a3c8e210a5b99
TCMooney <tcmooney88@gmail.com>
2019-01-11 21:21:56 -0500

    fixed grid and added arrow component

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 
 import Icon from '../icon';
+import Arrow from '../arrow';
 
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
-                <label className="library-course__title">Problem Solving</label>
-                { Icon('fas fa-check', 'library-course__icon') }
-                {/* arrow component */}
+                <div className="library-course__title-arrow">
+                    <label className="library-course__title">Problem Solving</label>
+                    { Icon('fas fa-check', 'library-course__icon') }
+                </div>
+                <Arrow className="library-course__arrow"/>
                 {/* action button component */}
-                {Icon('fas fa-plus-circle', 'library-course__action')}
                 <div className="library-course__description">
                     <label>Course Description</label>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu dui efficitur, lacinia augue vel, congue dui. Vestibulum quis scelerisque lorem, non fermentum mi. Nunc id magna sollicitudin, viverra neque et, tincidunt mauris.</p>
