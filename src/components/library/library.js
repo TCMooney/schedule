496c358b93d433778f6b4f7233c92089df92f33a
TCMooney <tcmooney88@gmail.com>
2019-01-12 19:44:57 -0500

    added fonts, font variables, color variables, styled library course__title, and gave library grid an inset of 100px

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -21,7 +21,7 @@ class Library extends Component {
     render() {
        return (
             <div className="library">
-                <h1 className="library__title">My Library</h1>
+                <h1 className="library__title">Course Library</h1>
                 { this.renderCourses() }
             </div>
        );
