694959c51d6f003dbaa763b7285e64e23f70fdcb
TCMooney <tcmooney88@gmail.com>
2019-01-14 19:22:08 -0500

    styled schedule course component and refactored schedule grid temoplate rows

--- src/components/action.js
+++ src/components/action.js
@@ -32,13 +32,4 @@ class Action extends Component {
     }
 }
 
-//         return(
-//             <button onClick={() => this.props.onClick()}  className={`${this.props.className} action`}>
-//                 { Icon('fas fa-plus-circle')}
-//                 {/* fas fa-times-circle */}
-//             </button>
-//         )
-//     }
-// }
-
 export default Action;
